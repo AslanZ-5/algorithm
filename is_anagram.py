e97883bf19f0dfccda61df325bb7bfb7eda40220
Aslan <asl.zurabov@gmail.com>
2021-11-26 16:34:20 +0300

    is anagram part 2

--- is_anagram.py
+++ is_anagram.py
@@ -1,5 +1,29 @@
-a = 'fairy tales'.lower().replace(' ','')
-b = 'rail safety'.lower().replace(' ','')
-print(sorted(a) == sorted(b))
+a = 'fairy tales'
+b = 'rail safety'
+# print(sorted(a) == sorted(b))
 
 
+def is_anagram1(s1, s2):
+    s1 = s1.lower().replace(' ', '')
+    s2 = s2.lower().replace(' ', '')
+    if len(s1) != len(s2):
+        return False
+    ht = dict()
+    for i in s1:
+        if i in ht:
+            ht[i] += 1
+        else:
+            ht[i] = 1
+    for i in s2:
+        if i in ht:
+            ht[i] -= 1
+        else:
+            ht[i] = 1
+    for i in ht:
+        if ht[i] != 0:
+            return False
+    return True
+
+
+
+print(is_anagram1(a, b))
