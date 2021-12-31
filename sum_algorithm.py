0f39bc06d5ec6c0cbd2cb728e0c4eb62dcad9d72
Aslan <asl.zurabov@gmail.com>
2021-12-31 22:25:23 +0300

    sum algorithms

---
+++ sum_algorithm.py
@@ -0,0 +1,26 @@
+import timeit
+
+"""take an input of n and return the sum of the numbers from 0 to n
+"""
+
+
+def sum1(n):
+    sum = 0
+    for i in range(1, n + 1):
+        sum += i
+    return sum
+
+
+def sum2(n):
+    return (n * (n + 1)) / 2
+
+
+start = timeit.timeit()
+sum1(100)
+end = timeit.timeit()
+print('sum1:',end - start)
+
+start = timeit.timeit()
+sum2(100)
+end = timeit.timeit()
+print('sum2:',end - start)
