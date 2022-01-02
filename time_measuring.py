0b52768017ad00a26d79664ec55437203377f10d
Aslan <asl.zurabov@gmail.com>
2022-01-02 20:27:17 +0300

    big O

--- time_measuring.py
+++ time_measuring.py
@@ -41,3 +41,62 @@ def func_quad(lst):
       You can see how dangerous this can get for very large inputs! this is why Big-O is so important to be aware of!
     - hence, the input of 3 gives us 9 outputs iterations.
     """
+
+
+# Calculating Scale of Big-O
+"""
+insignificant terms drop out of Big-O notation
+When it comes to Big O notation we only care about the most significant terms, remember as the input grows larger only the 
+fastest growing terms will matter.
+    - like taking towards infinity 
+"""
+
+
+def print_once(lst):
+    """Prints all items once O(n)"""
+    for val in lst:
+        print(val)
+
+
+def print_3(lst):
+    """
+    Prints all items three times
+    This algorithm will run 3 times for each n, so this becomes an order of 3(n). It is still linear
+    More importantly, 3(infinity) is not really different than (infinity) and we can drop insignificant
+    constants.
+    So this too is O(n).
+    """
+
+    for val in lst:
+        print(val)
+
+    for val in lst:
+        print(val)
+
+    for val in lst:
+        print(val)
+
+
+def comp(lst):
+    """
+    This is function prints the first item O(1) it is a constant
+    Then is prints the first 1/2 of the list o(n/2)
+    Then prints a string 10 times O(n) it is a constant
+    """
+    print (lst[0])
+
+    midpoint = len(lst)//2
+
+    for val in lst[:midpoint]:
+        print(val)
+    for x in range(10):
+        print('number')
+
+    """
+     O(1+ n/2 + 10)
+     An n gets larger and larger (scales up) you can easily see how the 1 and the 10 quickly begin to mean nothing.
+     And the //2 will begin to have no effect either 
+     End up with O(n)
+    """
+
+
