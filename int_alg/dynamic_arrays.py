0a5573167d706238304210bf66ae3a5f73e1f40f
Aslan <asl.zurabov@gmail.com>
2022-01-05 09:01:24 +0300

    Largest Sum Contiguous Subarray

--- int_alg/dynamic_arrays.py
+++ int_alg/dynamic_arrays.py
@@ -48,14 +48,14 @@ def pair_sum(arr, sm):
     for i in arr:
         for j in arr:
             if i + j == sm:
-                a.append((min((i, j)),max((i,j))))
-    return '\n'.join(map(str,list(set(a))))
+                a.append((min((i, j)), max((i, j))))
+    return '\n'.join(map(str, list(set(a))))
 
 
-print(pair_sum([1, 3, 2, 2], 4))
+# print(pair_sum([1, 3, 2, 2], 4))
 
 
-def pair_sum_2(arr,k):
+def pair_sum_2(arr, k):
     if len(arr) < 2:
         return print('Too small')
     seen = set()
@@ -65,7 +65,26 @@ def pair_sum_2(arr,k):
         if target not in seen:
             seen.add(num)
         else:
-            output.add((min(num,target),max(num,target)))
-    print ('\n'.join(map(str,list(output))))
+            output.add((min(num, target), max(num, target)))
+    print('\n'.join(map(str, list(output))))
 
-pair_sum_2([2,2,1,3],4)
+
+# pair_sum_2([2, 2, 1, 3], 4)
+
+
+# Largest Subarray Sum Problem
+"""Take an array with positive and negative integers and
+find the maximum sum of that array"""
+
+def largest(arr):
+    if len(arr) == 0:
+        return print('too small')
+    max_sum = current_sum = arr[0]
+
+    for num in arr[1:]:
+        current_sum = max(current_sum + num, num)
+        max_sum = max(current_sum, max_sum)
+    return max_sum
+
+
+print(largest([-2,-3,4,-1,-2,1,5,-3]))
