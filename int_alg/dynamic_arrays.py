3f4a21d8b0e581383399c4a1375e43a88577a8ff
Aslan <asl.zurabov@gmail.com>
2022-01-09 09:30:05 +0300

    common element in two arrays

--- int_alg/dynamic_arrays.py
+++ int_alg/dynamic_arrays.py
@@ -170,7 +170,35 @@ def rotation_arr(list1, list2):
             return False
     return True
 
-a = [0,0,0,4,5,6,7]
-b = [4,5,6,7,0,0,0]
 
-print(rotation_arr(a,b))
+a = [0, 0, 0, 4, 5, 6, 7]
+b = [4, 5, 6, 7, 0, 0, 0]
+
+print(rotation_arr(a, b))
+
+# Array Common Elements
+"""
+Common Elements in Two Sorted Arrays 
+return the common elements (as an array) between two sorted arrays of integers (ascending order).
+Example: The common elements between [1,3,4,6,7,9] and [1,2,4,5,9,10]
+are [1,4,9]
+
+"""
+
+def common_element(ls1,ls2):
+    p1 = 0
+    p2 = 0
+    result = []
+
+    while p1 < len(ls1) and p2 < len(ls2):
+        if ls1[p1] == ls2[p2]:
+            result.append(ls1[p1])
+            p1 += 1
+            p2 += 1
+        elif ls1[p1] > ls2[p2]:
+            p2 += 1
+        else:
+            p1 += 1
+    return result
+
+print(common_element([1,3,4,6,7,9] , [1,2,4,5,9,10]))
