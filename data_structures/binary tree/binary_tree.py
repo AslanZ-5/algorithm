234d7036108c7f975bd1db351024f4cc1040d541
мансур зурабов <asl.zurabov@gmail.com>
2022-02-16 11:19:07 +0300

    Binary Tree

---
+++ data_structures/binary tree/binary_tree.py
@@ -0,0 +1,28 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinaryTree(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+
+#            1
+#         /     \
+#        2        3
+#      /   \     /  \
+#     4     5   6    7
+#                     \
+#                      8
+# Set up tree:
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+tree.root.right.left = Node(6)
+tree.root.right.right = Node(7)
+tree.root.right.right.right = Node(8)
