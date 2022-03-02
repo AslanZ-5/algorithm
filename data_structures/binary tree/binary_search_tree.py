68e23420c2aedfe8e4915cb630e2c0cb08501260
мансур зурабов <asl.zurabov@gmail.com>
2022-03-02 11:52:10 +0300

    Data Structure: Binary Search Trees --> Checking the BST Property

--- data_structures/binary tree/binary_search_tree.py
+++ data_structures/binary tree/binary_search_tree.py
@@ -1,9 +1,9 @@
-
 class Node(object):
     def __init__(self, data=None):
         self.data = data
         self.left = None
         self.right = None
+
 
 class BinarySearchTree:
     def __init__(self):
@@ -14,6 +14,7 @@ class BinarySearchTree:
             self.root = Node(data)
         else:
             self._insert(data, self.root)
+
     def _insert(self, data, cur_node):
 
         if data < cur_node.data:
@@ -29,7 +30,17 @@ class BinarySearchTree:
         else:
             print('Value is already present is tree.')
 
-    def find(self,data):
+    def inorder_print_tree(self):
+        if self.root:
+            self._inorder_print_tree(self.root)
+
+    def _inorder_print_tree(self, cur_node):
+        if cur_node:
+            self._inorder_print_tree(cur_node.left)
+            print(str(cur_node.data))
+            self._inorder_print_tree(cur_node.right)
+
+    def find(self, data):
         if self.root:
             is_found = self._find(data, self.root)
             if is_found:
@@ -46,14 +57,35 @@ class BinarySearchTree:
         if data == cur_node.data:
             return True
 
+    def is_bst_satisfied(self):
+        def helper(node, lower=float('-inf'), upper=float('inf')):
+            if not node:
+                return True
+            val = node.data
+            if val <= lower or val >= upper:
+                return False
+            if not helper(node.right, val, upper):
+                return False
+            if not helper(node.left, lower, val):
+                return False
+            return True
+        return helper(self.root)
 
 bst = BinarySearchTree()
-bst.insert(4)
-bst.insert(2)
 bst.insert(8)
-bst.insert(5)
+bst.insert(3)
 bst.insert(10)
+bst.insert(1)
+bst.insert(6)
+bst.insert(9)
+bst.insert(11)
+
+
+bst2 = BinarySearchTree()
+bst2.root = Node(1)
+bst2.root.left = Node(2)
+bst2.root.right = Node(3)
 # print(bst.root.left.left)
 
-print(bst.find(1))
-
+print(bst.is_bst_satisfied())
+print(bst2.is_bst_satisfied())
